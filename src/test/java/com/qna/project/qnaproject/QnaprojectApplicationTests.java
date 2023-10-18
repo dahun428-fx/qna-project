ad970975cbaa6fcdfba50583def562c27e1ceeb1
**** <*****>
2023-10-18 10:23:42 +0900

    first commit

---
+++ src/test/java/com/qna/project/qnaproject/QnaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qna.project.qnaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
